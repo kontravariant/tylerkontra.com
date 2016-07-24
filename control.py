0d019c50f868e07194eba5719aaf3b4489d47eb6
Tyler Kontra <tkontra551@gmail.com>
2016-07-23 23:17:29 -0400

    Functional flask site, in own git directory within virtualenv.

--- control.py
+++ control.py
@@ -5,7 +5,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 import json, requests, itertools
 from titlecase import titlecase
-import datetime
 
 # Setup Flask app.
 app = Flask(__name__)
@@ -23,10 +22,10 @@ def static_proxy(path):
 
 @app.route('/pages/projects.html')
 def projects():
-    headers = {'Authorization': 'token %s' % '4ddacff158ceb607f1a6740c675e50af6fc5808a'}
+
     url = 'https://api.github.com/users/tylerkontra/repos'
     res = []
-    r = requests.get(url,headers=headers)
+    r = requests.get(url)
     print(r.text)
     data = json.loads(r.text)
     for dict in r.json():
@@ -36,28 +35,7 @@ def projects():
         res.append([repo_name, repo_url, repo_dsc])
         res = sorted(res)
 
-    activity_url = 'https://api.github.com/users/tylerkontra/events'
-    req = requests.get(activity_url, headers=headers)
-
-    acts = json.loads(req.text)
-    print(acts)
-    i = 0
-    report = []
-    for event in acts:
-        type = (event['type'])
-        date = (event['created_at'])
-        date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
-        date = datetime.datetime.date(date).strftime("%Y-%m-%d")
-        repo = (event['repo']['name']).split('/')[1]
-        if type == 'PullRequestEvent':
-            title = event['payload']['pull_request']['title']
-            act = [type, date, repo, title]
-            report.append(act)
-        else:
-            act = [type, date, repo,'']
-            report.append(act)
-
-    return render_template('projects.html',repos=res,report=report)
+    return render_template('projects.html',repos=res)
 
 if __name__ == '__main__':
   app.run()
