9323738812b6143e6b0c8dcd5004a1e56aeaf584
tylerkontra <tylerkontra551@gmail.com>
2016-07-23 23:03:54 -0400

    Added activity tracker to Projects page.

--- code/apitest.py
+++ code/apitest.py
@@ -1,17 +1,60 @@
+import datetime
+
 import json, requests
-from titlecase import titlecase
+
+headers = {'Authorization': 'token %s' % '4ddacff158ceb607f1a6740c675e50af6fc5808a'}
+
 
 url = 'https://api.github.com/users/tylerkontra/repos'
 
 res = []
-r = requests.get(url)
+r = requests.get(url, headers=headers)
 print(r.text)
 data = json.loads(r.text)
-for dict in r.json():
-    repo_name = titlecase(dict.get('name'))
-    repo_url = (dict.get('html_url'))
-    repo_dsc = (dict.get('description').capitalize())
+for iter in r.json():
+    repo_name = (iter.get('name'))
+    repo_url = (iter.get('html_url'))
+    repo_dsc = (iter.get('description').capitalize())
     res.append([repo_name,repo_url,repo_dsc])
     res = sorted(res)
 
 print(res)
+
+activity_url = 'https://api.github.com/users/tylerkontra/events'
+req = requests.get(activity_url, headers=headers)
+
+
+def id_generator(iter,targets):
+    for k, v in iter.items():
+        if k in targets:
+            yield v
+        elif isinstance(v,(dict)):
+            for id_val in id_generator(v,targets):
+                yield id_val
+
+
+acts = json.loads(req.text)
+print(acts)
+i = 0
+report=[]
+for event in acts:
+    type=(event['type'])
+    date=(event['created_at'])
+    date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+    date = datetime.datetime.date(date).strftime("%Y-%m-%d")
+    repo=(event['repo']['name']).split('/')[1]
+    if type == 'PullRequestEvent':
+        title = event['payload']['pull_request']['title']
+        act = [type, date, repo,title]
+        report.append(act)
+    else:
+        print(type,date,repo)
+        act = [type, date, repo]
+        report.append(act)
+print(report)
+'''
+
+                print(repo)
+                i += 1
+                print(message)
+'''
